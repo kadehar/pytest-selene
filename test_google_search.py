2cb372e35cb1c818c7d5c4e7b3af6636a009624b
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2022-06-15 16:35:43 +0300

    #2 Add browser size using pytest fixture

--- test_google_search.py
+++ test_google_search.py
@@ -1,10 +1,15 @@
 from selene.support.shared import browser
 from selene import be, have
+import pytest
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup():
+    browser.open('https://google.com')
+    browser.driver.set_window_size(width=1920, height=1080)
 
 
 def test_able_to_find_selene_in_google():
-    browser.open('https://google.com')
-
     query = browser.element('[name="q"]')
     query.should(be.blank).type('selene').press_enter()
 
