122759b9b0f9acf3fb3b534329608794a53a9240
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2022-06-15 17:00:51 +0300

    #3 Add negative test

--- test_google_search.py
+++ test_google_search.py
@@ -1,17 +1,31 @@
+from selene.core.condition import Condition
 from selene.support.shared import browser
 from selene import be, have
 import pytest
 
+selene_search_query = 'selene'
+invalid_search_query = 'gfdbvnthohg'
+
 
 @pytest.fixture(scope='session', autouse=True)
 def setup():
-    browser.open('https://google.com')
+    browser.open('data:')
     browser.driver.set_window_size(width=1920, height=1080)
 
 
 def test_able_to_find_selene_in_google():
+    search_for(text=selene_search_query,
+               condition=have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_unable_to_find_invalid_search_result_in_google():
+    search_for(text=invalid_search_query, condition=have.no.text(invalid_search_query))
+
+
+def search_for(text: str, condition: Condition):
+    browser.open('https://google.com')
     query = browser.element('[name="q"]')
-    query.should(be.blank).type('selene').press_enter()
+    query.should(be.blank).type(text).press_enter()
 
     search = browser.element('#search')
-    search.should(have.text('User-oriented Web UI browser tests in Python'))
+    search.should(condition)
