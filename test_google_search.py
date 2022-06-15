ac1b3b20c5d0f60553cfaea2206d0d573f96f73b
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2022-06-15 17:03:47 +0300

    #3 Added some params

--- test_google_search.py
+++ test_google_search.py
@@ -5,6 +5,7 @@ import pytest
 
 selene_search_query = 'selene'
 invalid_search_query = 'gfdbvnthohg'
+selene_search_result = 'User-oriented Web UI browser tests in Python'
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -14,12 +15,11 @@ def setup():
 
 
 def test_able_to_find_selene_in_google():
-    search_for(text=selene_search_query,
-               condition=have.text('User-oriented Web UI browser tests in Python'))
+    search_for(text=selene_search_query, condition=have.text(selene_search_query))
 
 
 def test_unable_to_find_invalid_search_result_in_google():
-    search_for(text=invalid_search_query, condition=have.no.text(invalid_search_query))
+    search_for(text=invalid_search_query, condition=have.no.texts())
 
 
 def search_for(text: str, condition: Condition):
